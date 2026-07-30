861fe1eb04937ddc18b580282ab7f33b4418f9a2
lucasmanuelcastro-cpu <lucasmanuelcastro@gmail.com>
2026-07-30 14:44:27 -0300

    Update URL_SCRIPT to new Google Script URL

--- config.js
+++ config.js
@@ -8,4 +8,4 @@
  * const URL_SCRIPT = "https://script.google.com/macros/s/AKfycb.../exec";
  * ============================================================
  */
-const URL_SCRIPT = "https://script.google.com/macros/s/AKfycbwnnkxJjClqUaueTSg76jlw-6RDX4hgkDni_xOINxJYQWfyBavftyCV7tmH2nh1NrNH/exec";
+const URL_SCRIPT = "https://script.google.com/macros/s/AKfycbwjK7bMuZu9qhlbzr9uGSZep2RVNR2Sd1UaZXwA447Xna7VV5pg552_mYZBTVKybxL6/exec";
