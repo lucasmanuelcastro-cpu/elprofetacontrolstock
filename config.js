845d2319d571c1867ff53eaf145823e8d802e237
lucasmanuelcastro-cpu <lucasmanuelcastro@gmail.com>
2026-07-30 15:11:19 -0300

    Update Google Apps Script URL in config.js

--- config.js
+++ config.js
@@ -1,11 +1,5 @@
 /**
  * CONFIG.JS - El Profeta
- * ============================================================
- * ÚNICO lugar donde tenés que pegar la URL de tu Google Apps
- * Script una vez que lo despliegues (ver INSTALACION.md).
- *
- * Ejemplo:
- * const URL_SCRIPT = "https://script.google.com/macros/s/AKfycb.../exec";
- * ============================================================
+
  */
-const URL_SCRIPT = "1";
+const URL_SCRIPT = "https://script.google.com/macros/s/AKfycbwjK7bMuZu9qhlbzr9uGSZep2RVNR2Sd1UaZXwA447Xna7VV5pg552_mYZBTVKybxL6/exec";
