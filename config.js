0526cc0990ba62f69cbbb0b0d722d641fffb6970
lucasmanuelcastro-cpu <lucasmanuelcastro@gmail.com>
2026-08-03 20:31:58 -0300

    Update config.js

--- config.js
+++ config.js
@@ -2,4 +2,4 @@
  * CONFIG.JS - El Profeta
 
  */
-const URL_SCRIPT = "https://script.google.com/macros/s/AKfycbxEHuMvBC3YqTcnfkd-_mO_RvI53fkpo3AQVrIsDXGPtXOmAh2iWL700bvOQ9eX8GR8/exec";
+const URL_SCRIPT = "https://script.google.com/macros/s/AKfycbwzQukQjm7Zcb_bUc6WrB0qfoEErd54qAWJo2SKJnJ6-JarJATzH5z3npWCa79HMrT7/exec";
