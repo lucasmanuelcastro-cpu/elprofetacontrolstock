93a02d11772651950e7b52f7849b8b43f4745355
lucasmanuelcastro-cpu <lucasmanuelcastro@gmail.com>
2026-08-04 12:07:24 -0300

    Update URL_SCRIPT to new Google Script URL

--- config.js
+++ config.js
@@ -2,4 +2,4 @@
  * CONFIG.JS - El Profeta
 
  */
-const URL_SCRIPT = "https://script.google.com/macros/s/AKfycbzJq1SbAjp1jBTsETeHeRPfdM7lBAKeKg2baqxvRYa2Nw3ETPIVbqy3ZW5c1wzc-KNq/exec";
+const URL_SCRIPT = "https://script.google.com/macros/s/AKfycbxLRY6KW0bEUK7SR8wVQrLBq3DN3DeG1qV_Eapit6j9Y3c95wLBzFKyskNajmaYDXqv/exec";
