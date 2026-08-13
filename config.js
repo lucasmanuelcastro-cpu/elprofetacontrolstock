21eaf40a596a2d03737b679b889074f47f93fbf4
lucasmanuelcastro-cpu <lucasmanuelcastro@gmail.com>
2026-08-13 10:54:44 -0300

    Fix URL_SCRIPT declaration in config.js

--- config.js
+++ config.js
@@ -2,5 +2,4 @@
  * CONFIG.JS - El Profeta
 
  */
-const URL_SCRIPT = "
-  https://script.google.com/macros/s/AKfycbzLKaEAApSUwD2szqfOjinO5cc5emGv5NJyME3Xm9EgtjRSeDUL_Wg9h0macseanrg/exec";
+const URL_SCRIPT = "https://script.google.com/macros/s/AKfycbzLKaEAApSUwD2szqfOjinO5cc5emGv5NJyME3Xm9EgtjRSeDUL_Wg9h0macseanrg/exec";
